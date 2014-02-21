1036983a7cca575595b25fa6bd88af6ca4533d13
huangwcchhs <huangwc@chhs.hcc.edu.tw>
2014-02-21 11:17:30 +0800

    upload : 竹苗區多元入學(竹苗區會考)學生匯入資料 路徑 : 教務作業>十二年國教>103(竹苗區會考)學生匯入資料 issue : 1."學生身分","身心障礙" : 呈現合併值 2."健康與體育","藝術與人文","綜合活動" : 呈現分數 3.曠課名稱 : 無儲存功能

---
+++ JH.HS.DataExchange.103/Map.designer.cs
@@ -0,0 +1,160 @@
+﻿namespace JH.HS.DataExchange._103
+{
+    partial class Map
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.key = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentTag = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.note = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnExit = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.comboBoxEx1 = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.key,
+            this.StudentTag,
+            this.note});
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(646, 244);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // key
+            // 
+            this.key.HeaderText = "類型";
+            this.key.Name = "key";
+            this.key.ReadOnly = true;
+            // 
+            // StudentTag
+            // 
+            this.StudentTag.HeaderText = "學生類別";
+            this.StudentTag.Name = "StudentTag";
+            this.StudentTag.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.StudentTag.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.StudentTag.Width = 350;
+            // 
+            // note
+            // 
+            this.note.HeaderText = "備註";
+            this.note.Name = "note";
+            this.note.Width = 90;
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnExit.Location = new System.Drawing.Point(583, 263);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnExit.TabIndex = 32;
+            this.btnExit.Text = "取消";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.BackColor = System.Drawing.Color.Transparent;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(502, 263);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(75, 23);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.TabIndex = 31;
+            this.buttonX1.Text = "確定";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // labelX1
+            // 
+            this.labelX1.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.Class = "";
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(13, 264);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(75, 23);
+            this.labelX1.TabIndex = 33;
+            this.labelX1.Text = "曠課名稱";
+            // 
+            // comboBoxEx1
+            // 
+            this.comboBoxEx1.DisplayMember = "Text";
+            this.comboBoxEx1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBoxEx1.FormattingEnabled = true;
+            this.comboBoxEx1.ItemHeight = 19;
+            this.comboBoxEx1.Location = new System.Drawing.Point(94, 263);
+            this.comboBoxEx1.Name = "comboBoxEx1";
+            this.comboBoxEx1.Size = new System.Drawing.Size(121, 25);
+            this.comboBoxEx1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.comboBoxEx1.TabIndex = 35;
+            // 
+            // Map
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 298);
+            this.Controls.Add(this.comboBoxEx1);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
+            this.Name = "Map";
+            this.Text = "Map";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DevComponents.DotNetBar.ButtonX btnExit;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn key;
+        private System.Windows.Forms.DataGridViewComboBoxColumn StudentTag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn note;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx comboBoxEx1;
+    }
+}
---
+++ JH.HS.DataExchange.103/SqlString.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JH.HS.DataExchange._103
+{
+    class SqlString
+    {
+        public static string Query1 = @"select student.id
+		,sss.健康與體育
+		,sss.藝術與人文
+		,sss.綜合活動
+		,disc.大功支數
+,disc.小功支數
+,disc.嘉獎支數
+,disc.大過支數
+,disc.小過支數
+,disc.警告支數
+,slr.服務學習時數_八上
+,slr.服務學習時數_八下
+,slr.服務學習時數_九上
+from student 
+left outer join class on student.ref_class_id=class.id
+left outer join 
+(
+select student.id
+			,avg(cast(xpath_string('<root>'||x1.score_info||'</root>','/root/Domains/Domain[@領域=''健康與體育'']/@成績') as float)) as ""健康與體育""
+			,avg(cast(xpath_string('<root>'||x1.score_info||'</root>','/root/Domains/Domain[@領域=''藝術與人文'']/@成績') as float)) as ""藝術與人文""
+			,avg(cast(xpath_string('<root>'||x1.score_info||'</root>','/root/Domains/Domain[@領域=''綜合活動'']/@成績') as float)) as ""綜合活動""
+		from 
+			student join class on student.ref_class_id=class.id and class.grade_year = 3
+			left join (
+								SELECT student.id
+	, ''||g1.SchoolYear as schoolyear1
+	, ''||g2.SchoolYear as schoolyear2
+	, ''||g3.SchoolYear as schoolyear3
+	, ''||g4.SchoolYear as schoolyear4
+	, ''||g5.SchoolYear as schoolyear5
+	, ''||g6.SchoolYear as schoolyear6
+FROM student 
+left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''7'' or @GradeYear=''1'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g1 on g1.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''7'' or @GradeYear=''1'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g2 on g2.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''8'' or @GradeYear=''2'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g3 on g3.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''8'' or @GradeYear=''2'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g4 on g4.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''9'' or @GradeYear=''3'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g5 on g5.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''9'' or @GradeYear=''3'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g6 on g6.id=student.id LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3)shistory on student.id=shistory.id
+			left join sems_subj_score as x1 on student.id=x1.ref_student_id
+			and (
+				(''||x1.school_year=shistory.schoolyear1 and x1.semester= 1)
+				or (''||x1.school_year=shistory.schoolyear2 and x1.semester= 2)
+				or (''||x1.school_year=shistory.schoolyear3 and x1.semester= 1)
+				or (''||x1.school_year=shistory.schoolyear4 and x1.semester= 2)
+				or (''||x1.school_year=shistory.schoolyear5 and x1.semester= 1)
+			)
+	group by student.id
+) as sss on student.id = sss.id
+left outer join 
+(
+	select 
+				student.id
+				,''||sum(CAST('0'|| xpath_string(x1.detail,'/Discipline/Merit[not(@ Cleared=""是"")]/@A') as integer)) as ""大功支數""
+				,''||sum(CAST('0'|| xpath_string(x1.detail,'/Discipline/Merit[not(@ Cleared=""是"")]/@B') as integer)) as ""小功支數""
+				,''||sum(CAST('0'|| xpath_string(x1.detail,'/Discipline/Merit[not(@ Cleared=""是"")]/@C') as integer)) as ""嘉獎支數""
+				,''||sum(CAST('0'|| xpath_string(x1.detail,'/Discipline/Demerit[not(@ Cleared=""是"")]/@A') as integer)) as ""大過支數""
+				,''||sum(CAST('0'|| xpath_string(x1.detail,'/Discipline/Demerit[not(@ Cleared=""是"")]/@B') as integer)) as ""小過支數""
+				,''||sum(CAST('0'|| xpath_string(x1.detail,'/Discipline/Demerit[not(@ Cleared=""是"")]/@C') as integer)) as ""警告支數""
+			from 
+				student join class on student.ref_class_id=class.id and class.grade_year = 3
+				left join (
+					SELECT student.id
+		, ''||g1.SchoolYear as schoolyear1
+		, ''||g2.SchoolYear as schoolyear2
+		, ''||g3.SchoolYear as schoolyear3
+		, ''||g4.SchoolYear as schoolyear4
+		, ''||g5.SchoolYear as schoolyear5
+		, ''||g6.SchoolYear as schoolyear6
+	FROM student 
+	left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''7'' or @GradeYear=''1'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g1 on g1.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''7'' or @GradeYear=''1'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g2 on g2.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''8'' or @GradeYear=''2'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g3 on g3.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''8'' or @GradeYear=''2'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g4 on g4.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''9'' or @GradeYear=''3'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g5 on g5.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''9'' or @GradeYear=''3'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g6 on g6.id=student.id LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3
+	)shistory on student.id=shistory.id
+				left join discipline as x1 on student.id=x1.ref_student_id
+				and (
+					(''||x1.school_year=shistory.schoolyear1 and x1.semester= 1)
+					or (''||x1.school_year=shistory.schoolyear2 and x1.semester= 2)
+					or (''||x1.school_year=shistory.schoolyear3 and x1.semester= 1)
+					or (''||x1.school_year=shistory.schoolyear4 and x1.semester= 2)
+					or (''||x1.school_year=shistory.schoolyear5 and x1.semester= 1)
+	                or (''||x1.school_year=shistory.schoolyear6 and x1.semester= 2)
+				)
+			where 
+				student.status = 1 
+				and class.grade_year = 3
+			group by student.id
+)as disc on student.id = disc.id
+left outer join 
+(
+	select student.id
+				,''||sum(CASE WHEN (''||x1.school_year=shistory.schoolyear3 and x1.semester= 1) THEN x1.hours ELSE 0 END) as ""服務學習時數_八上""
+				,''||sum(CASE WHEN (''||x1.school_year=shistory.schoolyear4 and x1.semester= 2) THEN x1.hours ELSE 0 END) as ""服務學習時數_八下""
+				,''||sum(CASE WHEN (''||x1.school_year=shistory.schoolyear5 and x1.semester= 1) THEN x1.hours ELSE 0 END) as ""服務學習時數_九上""
+			from 
+				student join class on student.ref_class_id=class.id and class.grade_year = 3
+				left join (
+					SELECT student.id
+		, ''||g1.SchoolYear as schoolyear1
+		, ''||g2.SchoolYear as schoolyear2
+		, ''||g3.SchoolYear as schoolyear3
+		, ''||g4.SchoolYear as schoolyear4
+		, ''||g5.SchoolYear as schoolyear5
+		, ''||g6.SchoolYear as schoolyear6
+	FROM student 
+	left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''7'' or @GradeYear=''1'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g1 on g1.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''7'' or @GradeYear=''1'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g2 on g2.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''8'' or @GradeYear=''2'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g3 on g3.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''8'' or @GradeYear=''2'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g4 on g4.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''9'' or @GradeYear=''3'' ) and (@Semester=''1'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g5 on g5.id=student.id left outer join (SELECT id, max(SchoolYear) as SchoolYear FROM xpath_table( 'id', '''<root>''||sems_history||''</root>''', 'student', '/root/History[ ( @GradeYear=''9'' or @GradeYear=''3'' ) and (@Semester=''2'')]/@SchoolYear', 'id IN ( select student.id from student LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3 )') AS tmp(id int, SchoolYear integer) group by id )as g6 on g6.id=student.id LEFT OUTER JOIN class ON student.ref_class_id = class.id WHERE student.status=1 AND class.grade_year = 3
+	)shistory on student.id=shistory.id
+				left join $k12.service.learning.record as x1 on (''||student.id)=x1.ref_student_id 
+				and (
+					(''||x1.school_year=shistory.schoolyear1 and x1.semester= 1)
+					or (''||x1.school_year=shistory.schoolyear2 and x1.semester= 2)
+					or (''||x1.school_year=shistory.schoolyear3 and x1.semester= 1)
+					or (''||x1.school_year=shistory.schoolyear4 and x1.semester= 2)
+					or (''||x1.school_year=shistory.schoolyear5 and x1.semester= 1)
+				)
+			group by student.id
+) as slr on student.id = slr.id
+where student.status = 1 and class.grade_year = 3";
+    }
+}
